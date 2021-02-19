e44693e699e39e6537a60ca173e9e3c5bbeb8215
Vilkas99 <victorium99@hotmail.com>
2021-02-19 17:51:58 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapaComponent } from './mapa/mapa.component';
+import { MarcadorService } from './marcador.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, MapaComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [MarcadorService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/mapa/mapa.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+import { MarcadorService } from '../marcador.service';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41],
+});
+L.Marker.prototype.options.icon = iconDefault;
+
+@Component({
+  selector: 'app-mapa',
+  templateUrl: './mapa.component.html',
+  styleUrls: ['./mapa.component.scss'],
+})
+export class MapaComponent implements AfterViewInit {
+  private mapa;
+  constructor(private marcadorServicio: MarcadorService) {}
+
+  private iniciarMapa(): void {
+    this.mapa = L.map('map', {
+      center: [39.8282, -98.5795],
+      zoom: 3,
+    });
+
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+
+    tiles.addTo(this.mapa);
+  }
+
+  ngAfterViewInit(): void {
+    this.iniciarMapa();
+    this.marcadorServicio.makeCapitalMarkers(this.mapa);
+  }
+}
